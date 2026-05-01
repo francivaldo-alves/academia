c6d7278fdd72d35255b2461c815c2f4744a21f38
Francivaldo <francivaldo.alves@gmail.com>
2026-05-01 20:03:22 -0300

    criado, services, controller,exception e specification nos filtros

---
+++ src/main/java/com/f3pro/mastersys/exception/EntityNotFoundException.java
@@ -0,0 +1,4 @@
+package com.f3pro.mastersys.exception;
+
+public class EntityNotFoundException {
+}
---
+++ src/main/java/com/f3pro/mastersys/specification/AlunoSpecification.java
@@ -0,0 +1,4 @@
+package com.f3pro.mastersys.specification;
+
+public class AlunoSpecification {
+}
